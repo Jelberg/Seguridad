5e92d4fddd467eaddbd2766a550143fa8a7f51ce
Jessica Elberg <elbergjessica@gmail.com>
2018-01-12 21:36:07 -0400

    Clases del Banco del cliente

---
+++ Clases BancoCliente/BancoCliente.java
@@ -0,0 +1,16 @@
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/* Se define la raiz de todo el proyecto*/
+@ApplicationPath("/")
+public class BancoCliente extends Application{
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet h = new HashSet<Class<?>>();
+        h.add( ServiciosCliente.class );
+        return h;
+    }
+}
